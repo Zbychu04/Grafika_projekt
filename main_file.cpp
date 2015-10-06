800a0df970b91d91f8dc1f188e8d948dc5b98dbc
Bartosz Zbytniewski <bartosz.zbytniewski@gmail.com>
2015-10-06 17:06:03 +0200

    Cos zmienialem, ale nie pamietam co

--- main_file.cpp
+++ main_file.cpp
@@ -11,15 +11,15 @@ void displayFrame(void) {
 
 
 int main(int argc, char* argv[]) {
-        glutInit(&argc, argv);
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-        glutInitWindowSize(800, 800);
-        glutInitWindowPosition(0, 0);
-        glutCreateWindow("Program OpenGL");
-        glutDisplayFunc(displayFrame);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Program OpenGL");
+	glutDisplayFunc(displayFrame);
 
-        //Tutaj kod inicjuj¹cy
+	//Tutaj kod inicjuj¹cy
 
-        glutMainLoop();
-        return 0;
+	glutMainLoop();
+	return 0;
 }
